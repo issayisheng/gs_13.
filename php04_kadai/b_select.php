3fe432d2f1bb3c2eaaaa3a0ad4aac953e72816bf
Issay Uchikoshi <issayuch@gmail.com>
2019-06-13 11:06:24 +0900

    04_kadai

---
+++ php04_kadai/b_select.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+
+include "b_funcs.php";
+$pdo = db_con();
+
+//２．データ登録SQL作成
+$stmt = $pdo->prepare("SELECT * FROM gs_an_table");
+$status = $stmt->execute();
+
+//３．データ表示
+$view = "";
+if ($status == false) {
+    sqlError($stmt);
+} else {
+    //Selectデータの数だけ自動でループしてくれる
+    //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
+    while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      
+      $view .= '<p>';
+      $view .= '<a href="b_detail.php?id='.$result["id"].'">';
+      $view .= $result["name"] . "（" . $result["email"]. "）" . $result["naiyou"];
+      $view .= '</a>';
+      $view .= ' ';
+      $view .= '<a href="b_delete.php?id='.$result["id"].'">';
+      $view .= "[削除]";
+      $view .= '</a>';
+      $view .= '</p>';
+
+    }
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>ブックマーク一覧</title>
+<link rel="stylesheet" href="css/range.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+<?php include("menu.php");?>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<div>
+    <div class="container jumbotron"><?=$view?></div>
+</div>
+<!-- Main[End] -->
+
+</body>
+</html>
---
+++ php04_kadai/session02.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+echo $_SESSION["name"];
+echo "<br";
+echo $_SESSION["age"];
+echo "<br";
+echo session_id();
+
+
+?>
